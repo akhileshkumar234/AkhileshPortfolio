b1afd03000cdf51653da97769579db11eb590e08
akhileshkumar234 <akhilesh13062001@gmail.com>
2022-01-22 20:48:39 +0530

    social icons updated

--- src/Components/Contacts/Contact.jsx
+++ src/Components/Contacts/Contact.jsx
@@ -38,10 +38,10 @@ export default function Contact() {
                 
                 <div className="col-12 col-sm-8 align-self-center">
                     <div className="text-center">
-                        <a className="btn btn-social-icon " href="http://google.com/+"><i className="fab fa-facebook-f fa-2x facebookcol"></i></a>
-                        <a className="btn btn-social-icon " href="http://www.facebook.com/profile.php?id="><i className="fab fa-twitter fa-2x twittercol"></i></a>
-                        <a className="btn btn-social-icon " href="http://www.linkedin.com/in/"><i className="fab fa-linkedin-in fa-2x linkedincol"></i></a>
-                        <a className="btn btn-social-icon " href="http://twitter.com/"><i className="fab fa-instagram fa-2x instacol"></i></a>
+                        <a className="btn btn-social-icon " href="https://www.facebook.com/profile.php?id=100009735406017"><i className="fab fa-facebook-f fa-2x facebookcol"></i></a>
+                        <a className="btn btn-social-icon " href="https://twitter.com/Akhiles08482458?t=7Y0noBnPMIPgQItUEt8IVA&s=08"><i className="fab fa-twitter fa-2x twittercol"></i></a>
+                        <a className="btn btn-social-icon " href="https://www.linkedin.com/in/akhilesh-kumar-1608851b3/"><i className="fab fa-linkedin-in fa-2x linkedincol"></i></a>
+                        <a className="btn btn-social-icon " href="https://www.instagram.com/awasthi9903/"><i className="fab fa-instagram fa-2x instacol"></i></a>
                     </div>
                 </div>
             </div>
